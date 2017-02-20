87471b1ba4be5670452a0aa793cba35f0193f883
MChen <ming.chen0919@gmail.com>
2017-02-19 23:06:38 -0500

    updates data manipulation

--- docs/data-manipulation.py
+++ docs/data-manipulation.py
@@ -14,12 +14,13 @@ sparksession = SparkSession(sc)
 # read data from csv
 # iris = sparksession.read.csv("data/iris.csv", inferSchema=True, header=True)
 mtcars = sparksession.read.csv("data/mtcars.csv", inferSchema=True, header=True)
-mtcars.show()
+mtcars.show(5)
 
 ## select columns by column names
-mtcars.select(['mpg', 'cyl']).show()
+mtcars.select(['mpg', 'cyl']).show(n=5)
 ## select columns by indices
 new_mtcars = mtcars.rdd.map(lambda x: [x[i] for i in range(1,6)]).toDF()
+new_mtcars.show(5)
 
 ## reset column names
 new_column_names = [mtcars.columns[i] for i in range(1, 6)]
@@ -37,6 +38,117 @@ new_column_names
 ori_column_names
 
 
-mtcars.rdd.map(lambda x: [x[i] for i in (1,3,6,7)]).toDF().show()
+mtcars.rdd.map(lambda x: [x[i] for i in range(1,6)]).toDF().show(n=5)
+mtcars.rdd.map(lambda x: [x[i] for i in (1,3,6,7)]).toDF().show(n=5)
+
+mtcars
+mtcars.withColumnRenamed('mpg', 'new_mpg').show(n=5)
+mtcars.show(n=5)
+.withCoumnRenamed('mpg', 'new_mpg').show(n=5)
 
 
+## get column number
+len(mtcars.columns)
+mtcars.count()
+
+mtcars.select(['carb']).distinct().show()
+
+training, test = mtcars.randomSplit(weights=[0.75, 0.25], seed=24)
+training.show()
+test.show()
+
+df1, df2, df3, df4 = mtcars.randomSplit(weights=[0.2, 0.2, 0.15, 0.25], seed=123)
+df1.count()
+df2.count()
+df3.count()
+df4.count()
+
+
+mtcars.show()
+
+iris.show(n=5)
+
+from pyspark.ml.feature import VectorIndexer
+iris.select(['species']).distinct().count()
+indexer = VectorIndexer(maxCategories=3, inputCol='species', outputCol='indexed_species')
+model = indexer.fit(iris)
+
+
+from pyspark.ml.feature import StringIndexer
+indexer = StringIndexer(inputCol='species', outputCol='indexed_species')
+model = indexer.fit(iris)
+model.transform(iris).show(n=5)
+model.transform(iris).select(['species', 'indexed_species']).distinct().show()
+
+
+species = iris.select(['species'])
+
+mtcars.show(n=5)
+
+mtcars.select(['carb']).distinct().count()
+
+indexer = VectorIndexer(maxCategories=6, inputCol='carb', outputCol='indexed_carb')
+model = indexer.fit(mtcars)
+
+mtcars.dtypes
+mtcars.schema
+
+from pyspark.ml.linalg import Vectors
+df = sparksession.createDataFrame([(Vectors.dense([-1.0, 0.0]),), (Vectors.dense([0.0, 1.0]),), (Vectors.dense([0.0, 2.0]),)], ["a"])
+df.show()
+indexer = VectorIndexer(maxCategories=2, inputCol="a", outputCol="indexed")
+model = indexer.fit(df)
+model.transform(df).show()
+
+
+
+advertising = sparksession.read.csv("data/Advertising.csv", inferSchema=True, header=True)
+
+advertising.show(n=5)
+
+from pyspark.ml.linalg import Vectors
+from pyspark.sql import Row
+
+transformed_data = advertising.rdd.map(lambda x: Row(featurs=Vectors.dense[x[0:3]], label=x[3])).toDF()
+
+
+# convert the data to dense vector
+def transData(row):
+    return Row(label=row["Sales"],
+               features=Vectors.dense([
+                                       row["TV"],
+                                       row["Radio"],
+                                       row["Newspaper"]]))
+                                       
+transformed = advertising.rdd.map(transData).toDF() 
+
+]
+from pyspark.ml.regression import LinearRegression
+lr = LinearRegression()
+model = lr.fit(transformed)
+model.transform(transformed).show(n=5)
+
+
+hsb2 = sparksession.read.csv("data/hsb2_modified.csv", inferSchema=True, header=True)
+hsb2.show(n=5)
+
+    
+from pyspark.ml.feature import StringIndexer
+indexer = StringIndexer(inputCol='gender', outputCol='indexed_gender')
+model = indexer.fit(hsb2)
+hsb2 = model.transform(hsb2)
+hsb2.show(n=5)
+
+
+new_hsb2 = hsb2.rdd.map(lambda row: Row(label=row['socst'],
+                                        features=Vectors.dense([
+                                            row['indexed_gender'],
+                                            row['race'],
+                                            row['ses'],
+                                            row['schtyp'],
+                                            row['prog'],
+                                            row['read'],
+                                            row['write'],
+                                            row['math'],
+                                            row['science']])
+                                        )).toDF().show(n=5)
